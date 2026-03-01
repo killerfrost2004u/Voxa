5f5087bf2008aaada0f9066946bbb8fc06cd936f
Ibrahim Yasser <hima.yasser2004@gmail.com>
2026-03-01 14:37:39 +0200

    update

--- backend/SQLFile1.sql
+++ backend/SQLFile1.sql
@@ -1,7 +1,7 @@
 USE DarkWolvesDB;
 GO
 
-SELECT FullName, Email, IsAdmin 
-FROM Users 
-WHERE Email = 'hima.yasser2004@gmail.com';
+ALTER TABLE JobApplications ADD Grammar_Rating NVARCHAR(50);
+ALTER TABLE JobApplications ADD Fluency_Rating NVARCHAR(50);
+ALTER TABLE JobApplications ADD Pronunciation_Rating NVARCHAR(50);
 GO
